8209c8d17cd380cf669a30bb4e0ca90a4546fa83
gauravgupta98 <gauravgupta1533@gmail.com>
2022-04-27 15:45:00 +0530

    Added model for Book and main function

---
+++ cmd/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gauravgupta98/book-management-api/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}
