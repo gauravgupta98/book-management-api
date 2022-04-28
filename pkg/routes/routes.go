e7a0f2f54afd439fd7f01102d2de096380ae3e95
gauravgupta98 <gauravgupta1533@gmail.com>
2022-04-28 07:42:30 +0530

    Added controllers for api routes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -8,7 +8,7 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{id}", controllers.GetBook).Methods("GET")
-	router.HandleFunc("/book/{id}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{id}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
